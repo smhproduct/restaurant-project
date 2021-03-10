669a868979b9cac51840920d701450bcf0f52374
Mohammad Akbar <mohammad.akbar2603@gmail.com>
2021-03-10 21:15:57 +0600

    Component Tree setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import React from 'react';
+import './App.css';
+import MainComponent from './Components/MainComponent';
 
 function App() {
   return (
     <div className="App">
-
+      <MainComponent />
     </div>
   );
 }
---
+++ src/Components/MainComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Body from './Body/Body';
+
+const MainComponent = () => {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    );
+}
+
+export default MainComponent;
