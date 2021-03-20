3830c0936d724e9779fae0099fde47c704901660
Mohammad Akbar <mohammad.akbar2603@gmail.com>
2021-03-20 11:09:37 +0600

    Designing menu items

--- src/Components/Body/MenuItem.js
+++ src/Components/Body/MenuItem.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardBody, CardTitle } from 'reactstrap';
 
 const MenuItem = props => {
     console.log(props);
     return (
         <div>
-
+            <Card style={{ margin: '5px' }}>
+                <CardBody>
+                    <CardImg
+                        alt={props.dish.name}
+                        src={props.dish.image}
+                        style={{ opacity: '0.7' }} />
+                    <CardImgOverlay>
+                        <CardTitle>{props.dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </CardBody>
+            </Card>
         </div>
     );
 }
