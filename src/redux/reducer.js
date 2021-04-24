967bd6da752585fe3a75c454d71f4ac4307151a9
Mohammad Akbar <mohammad.akbar2603@gmail.com>
2021-04-24 10:44:40 +0600

    Optimized the code in reducer.js, converted hardcoded actions into variables, combined multiple reducers into one

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,23 +1,29 @@
 import DISHES from '../data/dishes'
 import COMMENTS from '../data/comments'
+import { combineReducers } from 'redux';
+import * as actionTypes from './actionTypes';
 
-const initialState = {
-    dishes: DISHES,
-    comments: COMMENTS
-}
-
-export const Reducer = (state = initialState, action) => {
-    if (action.type === 'ADD_COMMENT') {
-        let comment = action.payload;
-        comment.id = state.comments.length;
-        comment.date = new Date().toDateString();
-        console.log(comment);
-
-        return {
-            ...state,
-            comments: state.comments.concat(comment)
-        }
+const dishReducer = (dishState = DISHES, action) => {
+    switch (action.type) {
+        default:
+            return dishState;
     }
-    return state;
-
 }
+
+const commentReducer = (commentState = COMMENTS, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_COMMENT:
+            let comment = action.payload;
+            comment.id = commentState.length;
+            comment.date = new Date().toDateString();
+            console.log(comment);
+            return commentState.concat(comment);
+        default:
+            return commentState;
+    }
+}
+
+export const Reducer = combineReducers({
+    dishes: dishReducer,
+    comments: commentReducer
+})
